d8eb7e5817888a3644a3084df324ea62451d94ad
gq.liu <gq.liu@trip.com>
2022-03-19 22:21:35 +0800

    第三次提交

--- login/views.py
+++ login/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import HttpResponse
 
+# Create your views here.
+
+user_list = []
 def index(request):
-	# return HttpResponse("Hello World!!`-`")
-	return render(request, "index.html")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        temp = {'user':username, 'pwd':password}
+        user_list.append(temp)
+    return render(request, 'index.html', {'data': user_list})
