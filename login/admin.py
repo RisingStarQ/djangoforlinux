ee287924a2531acaeb7a3e49357104c7261e311a
gq.liu <gq.liu@trip.com>
2022-04-16 10:28:23 +0800

    models.py文件练习

--- login/admin.py
+++ login/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import userInfo
+from .models import userInfo, Person, Group, Membership, Student
 
 # Register your models here.
 #若要在后台系统看到用户应用，必须先在admin中进行注册，告诉admin站点，请将userinfo的模型加入站点内，接受站点的管理。
 admin.site.register(userInfo)
+admin.site.register(Person)
+admin.site.register(Group)
+admin.site.register(Membership)
+admin.site.register(Student)
--- login/models.py
+++ login/models.py
@@ -10,3 +10,60 @@ class userInfo(models.Model):
 
 	def __str__(self):
 		return self.user
+
+'''
+多对多练习
+'''
+class Person(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
+class Group(models.Model):
+	name = models.CharField(max_length=128)
+	members = models.ManyToManyField(
+		Person,
+		through='Membership',
+		through_fields=('group','person'),
+	)
+
+	def __str__(self):
+		return self.name
+
+class Membership(models.Model):
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	person = models.ForeignKey(Person, on_delete=models.CASCADE)
+	inviter = models.ForeignKey(
+		Person,
+		on_delete=models.CASCADE,
+		related_name="membership_invites",
+	)
+	invite_reason = models.CharField(max_length=64)
+
+'''
+字段参数的练习
+'''
+class Student(models.Model):
+	FRESHMAN = 'FR'
+	SOPHOMORE = 'SO'
+	JUNIOR = 'JR'
+	SENIOR = 'SR'
+	YEAR_IN_SCHOOL_CHOICES = (
+		(FRESHMAN, 'Freshman'),
+		(SOPHOMORE, 'Sophomore'),
+		(JUNIOR, 'Junior'),
+		(SENIOR, 'Senior'),
+	)
+	name = models.CharField(max_length = 10)
+	year_in_school = models.CharField(
+		max_length = 2,
+		choices=YEAR_IN_SCHOOL_CHOICES,
+		default=FRESHMAN,
+	)
+
+	def is_upperclass(self):
+		return self.year_in_school in (self.JUNIOR, self.SENIOR)
+
+	def __str__(self):
+		return self.name
